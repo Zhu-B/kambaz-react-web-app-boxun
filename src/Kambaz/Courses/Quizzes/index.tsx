6f4de317a5c38fb8c6ceedefa02465366f0ff1a4
Zhu-B <zbx86242373@gmail.com>
2025-06-19 17:11:08 -0400

    anyway2

--- src/Kambaz/Courses/Quizzes/index.tsx
+++ src/Kambaz/Courses/Quizzes/index.tsx
@@ -1,7 +1,7 @@
-import { useParams } from "react-router";
+//import { useParams } from "react-router";
 
 export default function Quizzes() {
-    const { cid } = useParams();
+    //const { cid } = useParams();
     
     return (
         <div className="wd-quizzes">
